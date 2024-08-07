52e45a2bb926cf257b3b56ac8f8797ed76f051d8
princebambhrolia <bambhroliaprince26@gmail.com>
2024-08-07 14:38:35 +0530

    temputer

---
+++ project 1/ANGLE.CPP
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+int bs=100;
+int hra=10*100/100;
+int da=5*100/100;
+int ta=8*100/100;
+int area;
+area=bs+hra+da+ta;
+clrscr();
+printf("%d",area);
+getch();
+}
